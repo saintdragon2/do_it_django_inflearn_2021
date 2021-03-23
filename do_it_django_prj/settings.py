d94f0b168112d5b71e8de9148df1e873b24aa71d
SungYong Lee <saintdragon2@gmail.com>
2021-03-23 23:29:04 +0900

    모델을 생성하고, 시간 설정하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog',
+    'single_pages',
 ]
 
 MIDDLEWARE = [
@@ -105,13 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
