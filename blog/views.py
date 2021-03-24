86c5b7903d4960465cfc65c6b2ea223b0918519f
SungYong Lee <saintdragon2@gmail.com>
2021-03-24 19:40:32 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,15 @@ def index(request):
             'posts': posts,
         }
     )
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_page.html',
+        {
+            'post': post,
+        }
+    )
