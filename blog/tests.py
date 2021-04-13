8878e1c2856c66b683233a7013c6a1b05e469dbb
SungYong Lee <saintdragon2@gmail.com>
2021-04-13 23:15:51 +0900

    create post 페이지 만들기

--- blog/tests.py
+++ blog/tests.py
@@ -202,3 +202,11 @@ class TestView(TestCase):
         self.assertNotIn(self.post_002.title, main_area.text)
         self.assertNotIn(self.post_003.title, main_area.text)
 
+    def test_create_post(self):
+        response = self.client.get('/blog/create_post/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Create Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Create a New Post', main_area.text)
