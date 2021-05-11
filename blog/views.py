59b6e96b0a1b80844205493de5898cdeefbae00c
SungYong Lee <saintdragon2@gmail.com>
2021-05-11 23:10:31 +0900

    pagination으로 포스트를 나누어 보여주기

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ from .forms import CommentForm
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
