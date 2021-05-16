611fbdfd097c616ba3594c175dde936abb2b5c7e
SungYong Lee <saintdragon2@gmail.com>
2021-05-16 18:48:37 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -61,6 +61,13 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/143/e3445497d896a175/svg/{self.author.email}'
+
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
