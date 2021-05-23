ee3b00b8a87ae1f368872357bb27e38fbc3685dc
SungYong Lee <saintdragon2@gmail.com>
2021-05-23 21:04:40 +0900

    도커에 gunicorn 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -138,6 +138,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
